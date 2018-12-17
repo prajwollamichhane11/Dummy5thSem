c7c637d127c50440d90c8897df1434eb9c1e98da
sukubhattu <sukubhattusandesh8@gmail.com>
2018-12-17 21:54:24 +0545

    minor changes

--- knowhere/dummydata/urls.py
+++ knowhere/dummydata/urls.py
@@ -5,7 +5,9 @@ from .views import DummyListView
 
 
 urlpatterns = [
-	path('',DummyListView.as_view(), name='dummy_list'),
+	# path('',DummyListView.as_view(), name='dummy_list'),
+	path('', views.DummyList, name = 'dummy_list'),
+	path('<int:dummy_id>',views.DummyDetail, name = 'detail_view'),
 	path('form', views.trialform, name = 'form'),
 	# path('', views.dummylist, name = 'dummy_list'),
 	
--- knowhere/dummydata/views.py
+++ knowhere/dummydata/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Dummy
 # Create your views here.
 
-
+#this is class based view
 class DummyListView(ListView):
 	template_name = 'dummydata/dummy_list.html'
 	context_object_name = 'dummy_data_list'
@@ -17,6 +17,22 @@ class DummyListView(ListView):
 	'''the queryset in the ListView passes in the form of list so its name becomes dummy_list
 	because of the <model name>_list. if we want to override it to be any name as in the 
 	context we passed in function base view we can use context_object_name. '''
+
+#this is function based view
+def DummyList(request):
+	context = {
+		'dummy_data_list' : Dummy.objects.all()
+	}
+	return render(request, 'dummydata/dummy_list.html', context)
+
+def DummyDetail(request, dummy_id):
+	# return HttpResponse (f"you are looking at {dummy_id}")
+	dummy_data_detail = Dummy.objects.get(pk = dummy_id)
+	context = {
+	'dummy_data_detail' : dummy_data_detail
+	}
+	return render (request, 'dummydata/dummy_detail.html', context)
+
 @login_required
 def trialform(request):
 	form = DummyModelForm()
