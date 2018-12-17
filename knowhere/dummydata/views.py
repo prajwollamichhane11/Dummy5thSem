f5f3917e106f3b8e064664fb33bcadbda3751377
sukubhattu <sukubhattusandesh8@gmail.com>
2018-12-17 11:41:12 +0545

    user profie sucessfully created along with signals.py

--- knowhere/dummydata/views.py
+++ knowhere/dummydata/views.py
@@ -3,9 +3,11 @@ from django.views.generic import(
 	CreateView, ListView, DetailView, DeleteView, UpdateView)
 from django.http import HttpResponse
 from .forms import DummyModelForm
+from django.contrib.auth.decorators import login_required
 
 from .models import Dummy
 # Create your views here.
+
 
 class DummyListView(ListView):
 	template_name = 'dummydata/dummy_list.html'
@@ -15,7 +17,7 @@ class DummyListView(ListView):
 	'''the queryset in the ListView passes in the form of list so its name becomes dummy_list
 	because of the <model name>_list. if we want to override it to be any name as in the 
 	context we passed in function base view we can use context_object_name. '''
-
+@login_required
 def trialform(request):
 	form = DummyModelForm()
 	context= {
--- knowhere/users/views.py
+++ knowhere/users/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.urls import reverse
 
 def register(request):
 	if request.method == 'POST':
@@ -16,8 +15,6 @@ def register(request):
 			messages.success(request, f'form created for user {username}!')
 			'''now redirecting the user to login page'''
 			return redirect ('log_in')
-			
-			
 	else:
 		form = UserRegistrationForm()
 	return render(request, 'users/register.html', {'form':form})
