83f70ad113a62b47ef33e78293277350c6df43a6
sukubhattu <sukubhattusandesh8@gmail.com>
2018-12-18 08:37:35 +0545

    error at the signals.py removed

--- knowhere/users/signals.py
+++ knowhere/users/signals.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 from .models import Profile
 from django.dispatch import receiver
 
-@receiver(post_save, sender= User)
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-	if created:
-		profile.objects.create(user = instance)
+    if created:
+        Profile.objects.create(user = instance)
 
-@receiver(post_save, sender = User)
+@receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
 	instance.profile.save()
 
